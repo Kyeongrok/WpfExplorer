06989f51ee65868a20b077938c9aa7892e4abc18
kyeongrok.kim <kyeongrok.kim@e3view.com>
2025-11-28 19:08:25 +0900

    DarkWindow

---
+++ WpfExplorer/App.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using Jamesnet.Wpf.Controls;
+
+namespace WpfExplorer {
+    internal class App : JamesApplication
+    {
+        protected override Window CreateShell()
+        {
+            return new Window();
+        }
+    }
+    
+};
---
+++ WpfExplorer/Starter.cs
@@ -0,0 +1,12 @@
+﻿namespace WpfExplorer;
+
+internal class Starter
+{
+    [STAThread]
+    private static void Main(string[] args)
+    {
+        _ = new App().Run();
+        
+    }
+    
+}
