131cda59d940e79e1cfabd04aaf1bdc3c4505fe9
kyeongrok.kim <kyeongrok.kim@e3view.com>
2025-12-02 01:31:55 +0900

    6.14 SelectionCommand를 통해 Event Binding

--- WpfExplorer.Main/Local/ViewModels/MainContentViewModel.cs
+++ WpfExplorer.Main/Local/ViewModels/MainContentViewModel.cs
@@ -1,15 +1,25 @@
-﻿using WpfExplorer.Support.Local.Helpers;
+﻿using System.Windows;
+using System.Windows.Input;
+using CommunityToolkit.Mvvm.Input;
+using WpfExplorer.Support.Local.Helpers;
 using WpfExplorer.Support.Local.Models;
 
 namespace WpfExplorer.Main.Local.ViewModels;
 
 public class MainContentViewModel
 {
+    public ICommand FolderChangedCommand { get; init; }
     public List<FolderInfo> Roots { get; init; }
 
-    public MainContentViewModel(FileService fileService)
+    public MainContentViewModel(FileService _fileService)
     {
-        Roots = fileService.GenerateRootNodes();
+        FolderChangedCommand = new RelayCommand<FolderInfo>(FolderChanged);
+            Roots = _fileService.GenerateRootNodes();
+    }
+    
+    private void FolderChanged(FolderInfo folderInfo)
+    {
+        MessageBox.Show($"Selected: {folderInfo.Name}");
     }
         
 }
